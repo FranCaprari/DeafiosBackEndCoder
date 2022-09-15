b4a26433c53fe7bb6d187de5785baaa428153587
FranCaprari <francaprari25@gmail.com>
2022-09-15 20:18:51 -0300

    Agregando pug

--- Desafio5/Pug/main.js
+++ Desafio5/Pug/main.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const productosRouter = require('./productos');
+const Contenedor = require("./index");
+const constructor = new Contenedor("./productos.txt");
 
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use("/", express.static(__dirname + "/public"));
+
+app.set("view engine", "pug");
+app.get("/form", (req, res) => {
+    res.render("form")
+});
+app.get("/table", (req, res) => {
+    const productos = await (constructor.getAll());
+    res.render("table", productos)
+});
 
 app.use("/api/productos", productosRouter);
 
